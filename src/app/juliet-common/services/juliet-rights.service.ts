15719eff5edc0ebbdde9590a92dc51631888357c
Super d <superd001@gmail.com>
2016-12-23 19:04:14 +0100

    feat(auth) : fech auth status once

--- src/app/juliet-common/services/juliet-rights.service.ts
+++ src/app/juliet-common/services/juliet-rights.service.ts
@@ -1,21 +1,35 @@
+import { Observable } from 'rxjs/Observable';
 import { JulietAPIService } from './juliet-api.service';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class JulietRightsService {
 
-  constructor(public api:JulietAPIService) { }
+  public userIsAuthorized: Boolean = false;
+  private _authorizePacket;
 
-  public user_can(right:String, userId?:Number, target?:Number) {
-    if(!userId) userId = 0;
+  constructor(public api: JulietAPIService) { }
 
-    return this.api.get("Rights/"+right).map(
+  public user_can(right: String, userId?: Number, target?: Number) {
+    if (!userId) userId = 0;
+
+    return this.api.get("Rights/" + right).map(
       data => data
     );
   }
 
   public can_see_juliet() {
-    return this.user_can("USER_CAN_SEE_JULIET");
+    return this.userIsAuthorized === true ? Observable.of(this._authorizePacket) : this.user_can("USER_CAN_SEE_JULIET").map(
+      data => {
+        if (data.data === true) {
+          console.log("setting okay");
+          this.userIsAuthorized = true;
+          this._authorizePacket = data;
+        }
+
+        return data;
+      }
+    );
   }
 
 }
