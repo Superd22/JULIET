7b73b2b317f860206e92c6a04a819b0658bf1f5c
Super d <superd001@gmail.com>
2016-12-23 02:46:00 +0100

    feat(tag) : filter by tag category

--- src/app/tags/components/list/list.component.ts
+++ src/app/tags/components/list/list.component.ts
@@ -13,11 +13,12 @@ export class ListComponent implements OnInit {
     name: null,
     restricted: null,
     type: null,
+    cat: null,
   };
 
   private tagType = TagsType;
   public filteredTags = {
-    count:0,
+    count: 0,
   };
 
 
@@ -28,14 +29,20 @@ export class ListComponent implements OnInit {
     this.tagsAPI.getTags();
   }
 
+  private toggleCat(catName) {
+    this.filter.cat = this.filter.cat == catName ? null : catName;
+  }
+
+  private iconForCat(catName) {
+    return this.filter.cat == catName ? "check_box" : "check_box_outline_blank";
+  }
+
   private iconForFilter(filterName) {
     if (filterName == "restricted") return this.filter.restricted == 1 ? "check_box" : "check_box_outline_blank";
     else return this.filter.type == this.tagType[filterName] ? "check_box" : "check_box_outline_blank";
   }
 
   private toggleFilter(filterName) {
-    console.log("prut");
-    console.log(filterName);
     if (filterName == "restricted") this.filter.restricted = this.filter.restricted === null ? 1 : null;
     else this.filter.type = this.filter.type == this.tagType[filterName] ? null : this.tagType[filterName];
   }
