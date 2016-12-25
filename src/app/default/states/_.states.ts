c049f23e3d64c5c47472182164e51fba59341610
Super d <superd001@gmail.com>
2016-12-25 04:42:14 +0100

    feat(default) : base page

--- src/app/default/states/_.states.ts
+++ src/app/default/states/_.states.ts
@@ -1,5 +1,5 @@
 import { DefaultComponent } from './../components/default/default.component';
 
 export let STATES = [
-    {name:"secure.Default", url:"/", component: DefaultComponent}
+    { name: "secure.Default", url: "/", views: { 'content': {component:DefaultComponent} } }
 ]
