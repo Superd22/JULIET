3be5e145eac13c609a5922e6fc8fe013108260c4
Super d <superd001@gmail.com>
2017-01-30 13:50:36 +0100

    Chore

--- src/app/calendar/helpers.php
+++
@@ -1,7 +0,0 @@
-<?php
-  //require_once("../Rights/helper/Rights.php");
-  require_once("helper/aArrayAccess.php");
-  require_once("helper/aEvent.php");
-  require_once("helper/aSummary.php");
-  require_once("helper/Hash.php");
-?>
