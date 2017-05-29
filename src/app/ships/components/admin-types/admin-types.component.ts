10aeed161924efedd26cb9038f267035996de5c5
Superd22 <superd001@gmail.com>
2017-05-29 16:50:38 +0200

    Design(ships) custom scrollbar for admin editor

--- src/app/ships/components/admin-types/admin-types.component.ts
+++ src/app/ships/components/admin-types/admin-types.component.ts
@@ -1,20 +1,36 @@
+import { MalihuScrollbarService } from 'ngx-malihu-scrollbar';
 import { JulietShipsService } from './../../services/juliet-ships.service';
 import { ShipModel } from './../../interfaces/ship-model';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-admin-types',
   templateUrl: './admin-types.component.html',
   styleUrls: ['./admin-types.component.scss']
 })
-export class AdminTypesComponent implements OnInit {
+export class AdminTypesComponent implements OnInit, OnDestroy {
 
   public shipTypes: ShipModel[] = [];
   public activeShip: ShipModel = { id: -1, type: "", ico: "", parent: 0, name: "" };
-  constructor(private api: JulietShipsService) { }
+  constructor(private api: JulietShipsService, private mScrollbarService:MalihuScrollbarService) { }
 
   ngOnInit() {
     this.fetchTypes();
+    this.handleScrollBar(true);
+  }
+
+  ngOnDestroy() {
+    this.handleScrollBar(false);
+  }
+
+  /**
+   * Register or de-register the custom scroll-bar for the types of ships
+   * @param adding if we're adding the scroll bar or not (false for un-register)
+   */
+  private handleScrollBar(adding: boolean): void {
+    if(adding) 
+      this.mScrollbarService.initScrollbar('.ships-wraper', { axis: 'y', theme: 'dark-thick', scrollInertia: 0, scrollButtons: { enable: true } });
+    else this.mScrollbarService.destroy('.ships-wraper');
   }
 
   fetchTypes() {
